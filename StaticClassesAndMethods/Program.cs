3b54d161dec50601bc8f72fc86a17a502866eeed
eyup.karaman <eyup.karaman@ANK5-YZLMORT-01>
2022-09-07 14:21:58 +0300

    Add project files.

---
+++ StaticClassesAndMethods/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace StaticClassesAndMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Config.ConnectionString = "constr";
+            Config.SayHello();
+            if (!StringUtil.Validate(""))
+                Console.WriteLine("boş");
+            StringUtil util = new StringUtil();
+            int uzunluk = util.Lenght("Aşil");
+            Console.WriteLine(uzunluk);
+
+
+        }
+    }//heap memory, stack memory, queue
+    public static class Config
+    {
+        public static string ConnectionString { get; set; }
+        public static string SayHello()
+        {
+            return "Hello Eyyyübi ";
+        }
+    }
+    public class StringUtil
+    {
+
+        public static bool Validate(string value)
+        {
+            return !string.IsNullOrWhiteSpace(value);
+        }
+        public int Lenght(string value)
+        {
+            return value.Length;
+        }
+    }
+}
+// Ortak erişilen yerlerde kullanılır. Ender durumlarda görülür. Üstteki ygibi yapılırsa gg :D
